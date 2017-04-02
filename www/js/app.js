60d25cbce470862e6c068c582d1677bf3c0b7b90
Himanshu <himanshu.raj@spotdy.com>
2017-04-02 22:45:16 +0530

    last changes

--- www/js/app.js
+++ www/js/app.js
@@ -124,8 +124,12 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
+      $ionicPlatform.ready(function() {
+          setTimeout(function() {
+              navigator.splashscreen.hide();
+          }, 2000);
+      });
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
